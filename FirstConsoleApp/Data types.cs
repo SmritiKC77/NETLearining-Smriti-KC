d960d2ca867d7c989753ed1030f305b04e6ca0e9
Smritikc1205 <kcsmriti80@gmail.com>
2024-02-20 07:28:24 +0545

    Refactor files

---
+++ FirstConsoleApp/Data types.cs
@@ -0,0 +1,28 @@
+using System;
+class DataTypes{
+   public  void Learn()
+    {
+        byte a = 5;
+Console.WriteLine(a);
+short b = 23434;
+Console.WriteLine(b);
+
+
+
+float weight = 49.5f; // f represent float
+double weight2 = 49.3423432432432; //by default double linxa yedi float ra decimal ma xa vanye
+decimal weight3 = 49.545454545454545454545m;// m for decimal
+
+char name = 's';// single quote for character
+string Name = "Smriti";
+bool isFemale = false;
+DateTime date = DateTime.Now;// now le chai current date time dinxa
+Console.WriteLine(date);
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(weight);
+Console.WriteLine(weight2);
+Console.WriteLine(name);
+    }
+}
