866ad2b8152b7e08e9067260614fab9713756b28
Smritikc1205 <kcsmriti80@gmail.com>
2024-02-26 19:42:56 +0545

    added assignment

---
+++ Assignment_1/Greeter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+Console.WriteLine("Enter your Name:");
+string fullName = Console.ReadLine();
+string cfullName = fullName.ToUpper();
+
+Console.WriteLine("Enter your Date of Birth (YYYY/MM/DD):");
+string dobString = Console.ReadLine();
+DateTime DOB = DateTime.Parse(dobString);
+
+DateTime today = DateTime.Today;
+
+TimeSpan age = today - DOB;
+int totalDays = (int)age.TotalDays;
+
+int years = totalDays / 365;
+int months = (totalDays % 365) / 30;
+int weeks = ((totalDays % 365) % 30) / 7;
+int days = ((totalDays % 365) % 30) % 7;
+
+Console.WriteLine($"Hello, {cfullName} Ji");
+
+Console.WriteLine($"Your DOB: {DOB.ToString("dddd, dd MMMM yyyy")}.");
+
+Console.WriteLine($"Age as of now: {years} years {months} months {weeks} weeks and {days} days old. \n");
