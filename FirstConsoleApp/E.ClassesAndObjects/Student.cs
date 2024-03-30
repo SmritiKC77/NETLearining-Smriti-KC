cbe6554167083c92ecb0165a7c0d2ebdaa339dc1
SmritiKC <kcsmriti80@gmailcom>
2024-03-30 21:24:40 +0545

    refactor changes

--- FirstConsoleApp/E.ClassesAndObjects/Student.cs
+++ FirstConsoleApp/E.ClassesAndObjects/Student.cs
@@ -1,43 +1,50 @@
 using System;
 
 class Student
-{
-   //Default COnstructor 
-    public Student() // methos without any return type
+{    
+    // Default Constructor
+    public Student()
     {
 
     }
-   // prameterized Constructor
+    // Parameterized constructor
     public Student(string n, byte rn)
     {
-name = n;
-rollNumber= rn;
+        name = n;
+        RollNumber = rn;
     }
-    string name; // class vitra ko field haru ho 
-    byte rollNumber;// property is nothing but a field but we use it in a controllled manner
+
+    public Student(string n, byte rn, DateTime dob)
+    {
+        name = n;
+        RollNumber = rn;
+        Dob = dob;
+    }
+    
+    public string name;
+    byte rollNumber;
     public byte RollNumber
     {
-get{
-    return rollNumber; // get, set and value is a keyword(in blue)
-}
-set
-{
-    if(value<=100)
-    {
-        rollNumber= value;
+        get
+        {
+            return rollNumber;
+        }
+        set
+        {
+            if (value <= 100)
+            {
+                rollNumber = value;
+            }
+        }
     }
-}
-    }
-   // property ko intension nai outside of class access garna sakos
-    public string Address{get; set;} 
-    //means only read not write
-    public DateTime Dob{get;} // auto implemented property
-    DateTime dob;
-    public  virtual void DisplayInfo()
+    public string Address { get; set; }      //Auto implemented property
+    public DateTime Dob { get; }
+
+    public virtual void DisplayInfo()
     {
-        Console.WriteLine($"Student Name:{name}");
-        Console.WriteLine($"Student Roll Number:{rollNumber}");
-          Console.WriteLine($"Student DateOfBirth:{Dob}");
-            Console.WriteLine($"Address:{Address}");
+        Console.WriteLine($"Student Name: {name}");
+        Console.WriteLine($"Student Roll Number: {rollNumber}");
+        Console.WriteLine($"Student Dob: {Dob}");
+        Console.WriteLine($"Student Address: {Address}");
     }
 }
